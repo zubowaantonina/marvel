7540f33b8c53ffe15c9af5a9de9eefb00e866e5f
zubowaantonina <zubowaantonina@yandex.ru>
2024-08-24 14:25:38 +0300

    Data transformation

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import App from "./components/app/App";
 import MarvelService from "./services/MarvelService";
 import "./style/style.scss";
 
-const marvelService = new MarvelService();
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,25 +1,36 @@
-
-
-
-
 class MarvelService {
-    _apiBasic = 'https://gateway.marvel.com:443/v1/public/'
-    _apiKey = 'apikey=0b245886065ab63ab566b5a6ecf930b3'
+  _apiBasic = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=0b245886065ab63ab566b5a6ecf930b3";
 
-    getResource = async (url) => {
-        let res = await fetch(url);
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status ${res.status}`);
-        }
-        return await res.json();
+  getResource = async (url) => {
+    let res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status ${res.status}`);
+    }
+    return await res.json();
+  };
+  getAllCharacters = async () => {
+    const res = await this.getResource(
+      `${this._apiBasic}characters?limit=9&offset=210&${this._apiKey}`
+    );
+    return res.data.results.map(this._transformCharacter)
+    
+    
+  };
+  getCharacter = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBasic}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(res.data.results[0]);
+  };
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
     };
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBasic}characters?limit=9&offset=210&${this._apiKey}`);
-
-    }
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBasic}characters/${id}?${this._apiKey}`);
-
-    }
+  };
 }
 export default MarvelService;
