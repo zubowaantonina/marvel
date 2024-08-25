7f193e30aa0dba2c0cc63e3ab8dd59fb5c96e049
zubowaantonina <zubowaantonina@yandex.ru>
2024-08-25 13:39:49 +0300

    add char.id,

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -25,6 +25,7 @@ class MarvelService {
   };
   _transformCharacter = (char) => {
     return {
+      id: char.id,
       name: char.name,
       description: char.description,
       thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
