2e8026450e34e8abe1963939324622e70b8f8095
zubowaantonina <zubowaantonina@yandex.ru>
2024-08-25 12:56:07 +0300

    Component life cycle

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,27 +1,43 @@
 import { Component } from 'react';
 import MarvelService from "../../services/MarvelService";
-import './randomChar.scss';
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from '../errorMessage/ErrorMessage';
-import thor from '../../resources/img/thor.jpeg';
+import './randomChar.scss';
+
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar()
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.updateChar()
+    // }
     state = {
         char: {},
         loading: true,
         error: false
 
     }
-
+    componentDidMount() {
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 3000);
+      }
+    
+      componentWillUnmount() {
+        // clearInterval(this.timerId);
+      }
     marvelService = new MarvelService();
+
     onCharLoaded = (char) => {
-        this.setState({ char, loading: false });
+        this.setState({ char, 
+            loading: false,
+            error: false
+         });
     }
+    onCharLoading = () => {
+        this.setState({
+          loading: true,
+        });
+      };
     onError = () => {
         this.setState({
             loading: false,
@@ -30,6 +46,7 @@ class RandomChar extends Component {
     }
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             // .getAllCharacters()
             // .then(res=>console.log(res))
@@ -43,6 +60,7 @@ class RandomChar extends Component {
         const errorMessage = error ? <ErrorMessage /> : null;
         const spinner = loading ? <Spinner /> : null;
         const content = !(loading || error) ? <View char={char} /> : null;
+       
         return (
             <div className="randomchar">
                 {errorMessage}
@@ -56,7 +74,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -79,9 +97,13 @@ const View = ({ char }) => {
     }
     const { name, description, thumbnail, homepage, wiki } = char;
     const personalDescription = getDescription(description, 200)
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img" />
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">{personalDescription}</p>
--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,64 +1,81 @@
 const Spinner = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" style={{margin: '0 auto', background: 'none', display: 'block'}} width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-            <g transform="translate(80,50)">
-            <g transform="rotate(0)">
-            <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="1">
-            <animateTransform attributeName="transform" type="scale" begin="-0.875s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
-            <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.875s"></animate>
-            </circle>
-            </g>
-            </g><g transform="translate(71.21320343559643,71.21320343559643)">
-            <g transform="rotate(45)">
-            <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.875">
-            <animateTransform attributeName="transform" type="scale" begin="-0.75s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
-            <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.75s"></animate>
-            </circle>
-            </g>
-            </g><g transform="translate(50,80)">
-            <g transform="rotate(90)">
-            <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.75">
-            <animateTransform attributeName="transform" type="scale" begin="-0.625s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
-            <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.625s"></animate>
-            </circle>
-            </g>
-            </g><g transform="translate(28.786796564403577,71.21320343559643)">
-            <g transform="rotate(135)">
-            <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.625">
-            <animateTransform attributeName="transform" type="scale" begin="-0.5s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
-            <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.5s"></animate>
-            </circle>
-            </g>
-            </g><g transform="translate(20,50.00000000000001)">
-            <g transform="rotate(180)">
-            <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.5">
-            <animateTransform attributeName="transform" type="scale" begin="-0.375s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
-            <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.375s"></animate>
-            </circle>
-            </g>
-            </g><g transform="translate(28.78679656440357,28.786796564403577)">
-            <g transform="rotate(225)">
-            <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.375">
-            <animateTransform attributeName="transform" type="scale" begin="-0.25s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
-            <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.25s"></animate>
-            </circle>
-            </g>
-            </g><g transform="translate(49.99999999999999,20)">
-            <g transform="rotate(270)">
-            <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.25">
-            <animateTransform attributeName="transform" type="scale" begin="-0.125s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
-            <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.125s"></animate>
-            </circle>
-            </g>
-            </g><g transform="translate(71.21320343559643,28.78679656440357)">
-            <g transform="rotate(315)">
-            <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.125">
-            <animateTransform attributeName="transform" type="scale" begin="0s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
-            <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="0s"></animate>
-            </circle>
-            </g>
-            </g>
-        </svg>
+        // <svg xmlns="http://www.w3.org/2000/svg" style={{margin: '0 auto', background: 'none', display: 'block'}} width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+        //     <g transform="translate(80,50)">
+        //     <g transform="rotate(0)">
+        //     <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="1">
+        //     <animateTransform attributeName="transform" type="scale" begin="-0.875s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
+        //     <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.875s"></animate>
+        //     </circle>
+        //     </g>
+        //     </g><g transform="translate(71.21320343559643,71.21320343559643)">
+        //     <g transform="rotate(45)">
+        //     <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.875">
+        //     <animateTransform attributeName="transform" type="scale" begin="-0.75s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
+        //     <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.75s"></animate>
+        //     </circle>
+        //     </g>
+        //     </g><g transform="translate(50,80)">
+        //     <g transform="rotate(90)">
+        //     <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.75">
+        //     <animateTransform attributeName="transform" type="scale" begin="-0.625s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
+        //     <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.625s"></animate>
+        //     </circle>
+        //     </g>
+        //     </g><g transform="translate(28.786796564403577,71.21320343559643)">
+        //     <g transform="rotate(135)">
+        //     <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.625">
+        //     <animateTransform attributeName="transform" type="scale" begin="-0.5s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
+        //     <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.5s"></animate>
+        //     </circle>
+        //     </g>
+        //     </g><g transform="translate(20,50.00000000000001)">
+        //     <g transform="rotate(180)">
+        //     <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.5">
+        //     <animateTransform attributeName="transform" type="scale" begin="-0.375s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
+        //     <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.375s"></animate>
+        //     </circle>
+        //     </g>
+        //     </g><g transform="translate(28.78679656440357,28.786796564403577)">
+        //     <g transform="rotate(225)">
+        //     <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.375">
+        //     <animateTransform attributeName="transform" type="scale" begin="-0.25s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
+        //     <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.25s"></animate>
+        //     </circle>
+        //     </g>
+        //     </g><g transform="translate(49.99999999999999,20)">
+        //     <g transform="rotate(270)">
+        //     <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.25">
+        //     <animateTransform attributeName="transform" type="scale" begin="-0.125s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
+        //     <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.125s"></animate>
+        //     </circle>
+        //     </g>
+        //     </g><g transform="translate(71.21320343559643,28.78679656440357)">
+        //     <g transform="rotate(315)">
+        //     <circle cx="0" cy="0" r="7" fill="#1c4595" fillOpacity="0.125">
+        //     <animateTransform attributeName="transform" type="scale" begin="0s" values="1.5 1.5;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
+        //     <animate attributeName="fillOpacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="0s"></animate>
+        //     </circle>
+        //     </g>
+        //     </g>
+        // </svg>
+        <svg xmlns="http://www.w3.org/2000/svg"
+            style={{ margin: '0 auto', background: 'none', display: 'block' }} width="100px" height="100px"
+            viewBox="0 0 128 128" space="preserve"><rect x="0" y="0" fill="#FFFFFF" /><g>
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#000000" />
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#f8f8f8" transform="rotate(30 64 64)" />
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#e8e8e8" transform="rotate(60 64 64)" />
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#d4d4d4" transform="rotate(90 64 64)" />
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#bebebe" transform="rotate(120 64 64)" />
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#a6a6a6" transform="rotate(150 64 64)" />
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#8e8e8e" transform="rotate(180 64 64)" />
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#737373" transform="rotate(210 64 64)" />
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#5a5a5a" transform="rotate(240 64 64)" />
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#414141" transform="rotate(270 64 64)" />
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#2a2a2a" transform="rotate(300 64 64)" />
+                <path d="M61.5.05v18.52a45.2 45.2 0 0 0-15.52 3.62l-9.3-16.1A63.67 63.67 0 0 1 61.5.05z" fill="#151515" transform="rotate(330 64 64)" />
+                <animateTransform attributeName="transform" type="rotate" values="0 64 64;30 64 64;60 64 64;90 64 64;120 64 64;150 64 64;180 64 64;210 64 64;240 64 64;270 64 64;300 64 64;330 64 64"
+                    calcMode="discrete" dur="1080ms" repeatCount="indefinite"></animateTransform></g></svg>
     )
 }
 
